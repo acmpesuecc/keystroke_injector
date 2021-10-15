da0f91055ba81b35a3b3fc10b89c06131650d016
Mallika S <54077667+gall1frey@users.noreply.github.com>
2021-10-15 09:10:59 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,20 +1,58 @@
 #include<stdio.h>
 #include<string.h>
 
-char* your_code_here(char* full_data) {
+void interpret_keycode(char*);
+void write_string(char*);
+void shift_plus_char(char);
+void ctrl_plus_char(char);
+void ctrl_plus_shift_plus_char(char);
+void win_plus_char(char);
+void enter();
+void arrow_key(char);
 
+FILE *fptr;
+
+int main(int argc, char *argv[]){
+	printf("VERIFYING YOUR INTERPRETER...\n");
+	for(int i=0; i<3; i++){
+		char filename[10];
+		sprintf(filename,"test%d.txt",i);
+		fptr = fopen(filename,"r");
+		char *keycode;
+		fgets(keycode,500,fptr);
+		interpret_keycode(keycode);
+	}
+	return 0;
 }
 
-int main(){
-  char* full_data = "STRING HACKTOBERFEST";
-  char* res = your_code_here(full_data);
-  char* ans = "bleKeyboard.print(\"HACKTOBERFEST\");";
-  printf("FIRST BENCHMARK ")
-  if(!strcmp(res,ans))
-    printf("PASSED\n");
-  else
-    printf("FAILED\n");
-    char* full_data = "";
-    char* res = your_code_here(full_data);
-    char* ans = "bleKeyboard.print(\"Hello worldsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss\");";
+FILE *fptr;
+
+void interpret_keycode(char* code) {
+  /*YOUR CODE GOES HERE*/
+}
+
+void write_string(char* string_to_print) {
+	printf("\%P\%%s\%P\% ",string_to_print);
+}
+
+void shift_plus_char(char letter) {
+  printf("\%S\%%c\%S\% ", letter);
+}
+
+void ctrl_plus_char(char letter) {
+	printf("\%C\%%c\%C\% ", letter);
+}
+
+void ctrl_plus_shift_plus_char(char letter) {
+	printf("\%CS\%%c\%CS\% ", letter);
+}
+
+void win_plus_char(char letter) {
+	printf("\%W\%%c\%W\% ", letter);
+}
+void enter() {
+	printf("\%ENTER\% ");
+}
+void arrow_key(char dir) {
+	printf("\%A\%%c\%A\% ", dir);
 }
